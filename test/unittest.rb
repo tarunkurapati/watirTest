ee2cd59489f4c85cf0259c4c9dcdb1dc2c3c97f6
tarun <tarunkurapati@gmail.com>
2012-12-11 15:17:01 +0900

    for head

--- test/unittest.rb
+++ test/unittest.rb
@@ -1,10 +1,5 @@
 require 'rubygems'
 require 'watir-webdriver'
-require 'watir-webdriver'
-require 'headless'
-headless = Headless.new
-headless.start
 b = Watir::Browser.start 'www.google.com'
 puts b.title
 b.close
-headless.destroy
